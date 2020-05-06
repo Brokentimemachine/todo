50bd94680ddf5bcb9c8cf41746ae4dc7d40ad6b5
Adham Elbaz <ae5270@att.com>
2020-05-06 10:36:28 -0400

    commiting migrations

--- app.py
+++ app.py
@@ -1,30 +1,45 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+import sys
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://adham@localhost:5432/todoapp'
 db = SQLAlchemy(app)
 
-class Todo (db.Model):
-    __tablename__ = 'todos'
-    id = db.Column(db.Integer, primary_key = True)
-    description = db.Column(db.String(), nullable = False) 
-
-    def __repr__ (self):
-        return f'<Todo {self.id} {self.description}>'
-
-db.create_all()
+migrate = Migrate(app, db)
 
 
-@app.route('/todos/create', methods = ['POST'])
+class Todo(db.Model):
+  __tablename__ = 'todos'
+  id = db.Column(db.Integer, primary_key=True)
+  description = db.Column(db.String(), nullable=False)
+  completed = db.Column(db.Boolean, nullable=False, default=False)
+
+  def __repr__(self):
+    return f'<Todo {self.id} {self.description}>'
+
+@app.route('/todos/create', methods=['POST'])
 def create_todo():
-    description_data = request.form.get('description', '')
-    todo= Todo(description = description_data)
+  error = False
+  body = {}
+  try:
+    description = request.form.get_json()['description']
+    todo = Todo(description=description)
     db.session.add(todo)
     db.session.commit()
-    return redirect(url_for('index'))
-
+    body['description'] = todo.description
+  except:
+    error = True
+    db.session.rollback()
+    print(sys.exc_info())
+  finally:
+    db.session.close()
+  if error:
+    abort (400)
+  else:
+    return jsonify(body)
 
 @app.route('/')
 def index():
-    return render_template('index.html', data = Todo.query.all())
+  return render_template('index.html', data=Todo.query.all())
